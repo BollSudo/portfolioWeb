4fb301b113c2d5005d646474551a2c4b759ad6f5
Boll <bollsudomaster@gmail.com>
2024-11-28 18:05:26 +0100

    feat: implémentation d'un template de cartes pour un affichage d'information en blocs

---
+++ ressources/js/cartes.js
@@ -0,0 +1,24 @@
+
+
+const cartes = document.querySelectorAll(".carte");
+
+window.addEventListener("mousemove", (e) => {
+
+    cartes.forEach( (carte) => {
+        const blob = carte.querySelector(".blob");
+        const fakeblob = carte.querySelector(".fakeblob");
+
+        const rec = fakeblob.getBoundingClientRect();
+
+        blob.animate(
+            [{
+                transform: `translate(${e.clientX - rec.left - (rec.width / 2)}px,${e.clientY - rec.top - (rec.height / 2)}px)`,
+            }],
+            {
+                duration: 300,
+                fill: "forwards",
+            }
+        );
+
+    });
+});
