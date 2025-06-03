5ce28a71a303c3cdfda4fa08e706932f0d675f6b
BollSudo <bollsudomaster@gmail.com>
2025-06-04 00:32:14 +0200

    feat: update gerer

--- src/vue/competences/gerer.php
+++ src/vue/competences/gerer.php
@@ -1,5 +1,5 @@
-<link href="../ressources/scss/projet.css" rel="stylesheet">
 <link href="../ressources/scss/competences.css" rel="stylesheet">
+<link href="../ressources/scss/projet.css" rel="stylesheet">
 
 <?php require __DIR__ . "/boutonsNiveau.php" ?>
 
@@ -25,14 +25,66 @@
                 d'un cahier des charges client</h3>
 
             <p class="competenceDescription">
-                Dans le cadre d’un projet en groupe de cinq, j’ai participé à la création d’une base de données relationnelle à l’aide du SGBD Oracle, en utilisant l’interface en ligne de commande iSQL*Plus. À partir d’un cahier des charges fictif que j’ai contribué à rédiger, j’ai modélisé une base à l’aide d’un diagramme entité/association conçu avec le logiciel Looping. J’ai ensuite écrit des scripts SQL pour créer, modifier, insérer et supprimer les données de la base.
-                Ce projet m’a permis de mettre en œuvre les notions fondamentales des bases de données relationnelles, notamment les relations binaires et ternaires, les contraintes d’intégrité (domaines, inclusion, exclusion, fonctionnelles), les entités héritées ainsi que les clés primaires et étrangères. J’ai également utilisé DBeaver, un outil open source d’administration de base de données, pour importer les données initiales sous forme de tableaux temporaires et faciliter la structuration de la base.
+                Dans le cadre de projets SAE de première année, j’ai pu participer à la création d’une <b>base de données relationnelle</b> à l’aide du <i>SGBD Oracle</i>, en utilisant l’interface en ligne de commande <i>iSQL*Plus</i>. J’ai su <b>modéliser</b> une base de données à l’aide d’un diagramme entité/association conçu avec le logiciel <i>Looping</i>. J’ai ensuite écrit des <b>scripts SQL</b> pour créer, modifier, insérer et supprimer les données de la base.
+                Ces projets m’ont permis de mettre en œuvre les notions fondamentales des bases de données relationnelles, notamment les relations binaires et ternaires, les contraintes d’intégrité (domaines, inclusion, exclusion, fonctionnelles), la notion d'héritage et de clés primaires et étrangères. J’ai également utilisé <i>DBeaver</i>, un outil open source d’administration de base de données, pour importer les données initiales sous forme de tableaux temporaires et faciliter la structuration de la base de données.
             </p>
 
+            <div class="galleryContainer">
+                <div class="galleryItem php">
+                    <img src="../ressources/img/competences/ea.png"
+                         title="Modèle E/A d'une application de réservation de créneaux de jeux de rôles"
+                         onclick="openModal(this.src, this.title)"
+                         alt="EA JDR">
+                </div>
+                <div class="galleryItem php">
+                    <img src="../ressources/img/competences/eaSpoty.png"
+                         title="Modèle E/A d'une application type Spotify"
+                         onclick="openModal(this.src, this.title)"
+                         alt="EA Spotify">
+                </div>
+                <!-- Modal for Image Display -->
+                <div id="myModal" class="modal" onclick="closeModal()">
+                    <div class="titleConteneur">
+                        <h3 class="modal-title" id="modalTitle"></h3>
+                        <span class="close" onclick="closeModal()">&times;</span>
+                    </div>
+                    <img class="modal-content" id="modalImage"">
+                </div>
+            </div>
+
             <p class="competenceDescription">
-                En parallèle, j’ai manipulé un jeu de données brutes issues de l’application Spotify, fournies sous forme de fichiers CSV. Ces données ont été transformées et analysées à l’aide du tableur LibreOffice Calc. J’ai utilisé des outils de tri, de regroupement et de calcul pour extraire les modalités, les fréquences, les effectifs, ainsi que des indicateurs statistiques comme la moyenne, la médiane, les quartiles, l’écart-type et l’écart interquartile.
-                En fonction de la nature des variables (qualitatives ordinales ou nominales, quantitatives discrètes ou continues), j’ai adapté la restitution visuelle à travers différents supports : tableaux, diagrammes en secteurs, diagrammes en bâtons, histogrammes et boxplots. Ce travail m’a permis de mieux comprendre l’importance de l’organisation des données pour en faciliter l’analyse et la communication.
+                En parallèle, j’ai manipulé et réalisé des <b>études statistiques</b> sur un jeu de données brutes issues de l’application <i>Spotify</i>, fournies sous forme de fichiers <i>CSV</i>. Ces données ont été transformées et analysées à l’aide du tableur <i>LibreOffice Calc</i>. J’ai utilisé des outils de tri, de regroupement et de calcul pour extraire les modalités, les fréquences, les effectifs, ainsi que des indicateurs statistiques comme la moyenne, la médiane, les quartiles, l’écart-type et l’écart interquartile.
+                En fonction de la nature des variables (qualitatives ordinales ou nominales, quantitatives discrètes ou continues), j’ai adapté la <b>restitution visuelle</b> à travers différents supports : tableaux, diagrammes en secteurs, diagrammes en bâtons, histogrammes et boxplots. Ce travail m’a permis de mieux comprendre l’importance de l’organisation des données pour en faciliter l’analyse et la communication.
             </p>
+
+            <div class="galleryContainer">
+                <div class="galleryItem php">
+                    <img src="../ressources/img/competences/stats1.png"
+                         title="Statistiques sur les données de Spotify"
+                         onclick="openModal(this.src, this.title)"
+                         alt="stats1">
+                </div>
+                <div class="galleryItem php">
+                    <img src="../ressources/img/competences/stats4.png"
+                         title="Statistiques sur les données de Spotify"
+                         onclick="openModal(this.src, this.title)"
+                         alt="stats3">
+                </div>
+                <div class="galleryItem php">
+                    <img src="../ressources/img/competences/stats3.png"
+                         title="Statistiques sur les données de Spotify"
+                         onclick="openModal(this.src, this.title)"
+                         alt="stats4">
+                </div>
+                <!-- Modal for Image Display -->
+                <div id="myModal" class="modal" onclick="closeModal()">
+                    <div class="titleConteneur">
+                        <h3 class="modal-title" id="modalTitle"></h3>
+                        <span class="close" onclick="closeModal()">&times;</span>
+                    </div>
+                    <img class="modal-content" id="modalImage"">
+                </div>
+            </div>
 
             <div class="competenceListes">
                 <h4 class="acTitle">Apprentissages critiques</h4>
@@ -52,15 +104,15 @@
                 </p>
                 <p class="competenceDescription">
                     A travers divers projets, j'ai pu manipuler des <b>données hétérogènes</b>, en respectant la confidentialité. Par exemple, le projet
-                    <a href="">StudAdvisor</a> m'a permis de manipuler des données issues directement de l'IUT (LDAP et données anonymisées sur les notes) via une base de données sécurisée (en utilisant des requêtes SQL préparées), en gérant l'accès aux données selon les droits de l'utilisateur et le but de l'application. Le projet m'a également permis de restituer des données brutes (formats CSV) pour les transformer en données relationnelles et les gérer via une application web dynamique (en PHP) pour un affichage visuel et ergonomique.
+                    <a href="router.php?action=afficherProjet&controleur=base&numProjet=2">StudAdvisor</a> m'a permis de manipuler des données issues directement de l'IUT (LDAP et données anonymisées sur les notes) via une base de données sécurisée (en utilisant des requêtes SQL préparées), en gérant l'accès aux données selon les droits de l'utilisateur et le but de l'application. Le projet m'a également permis de restituer des données brutes (formats CSV) pour les transformer en données relationnelles et les gérer via une application web dynamique (en PHP) pour un affichage visuel et ergonomique.
                 </p>
                 <p class="competenceDescription">
-                    Par ailleurs, lors de mon stage de deuxième année au CIRAD avec le projet <a href="">Race against the Fly</a>, j'ai manipulé (extraire, convertir et génerer) des données issues de <b>résultats scientifiques</b> pour mettre en œuvre une <b>interface graphique</b> en <i>Python</i> avec des données de configurations (yaml) et des données de modélisaiton (csv). Cette application permet la saisie des données dans un <b>modèle scientifique</b>, l'affichage des résultats via l'interface graphique, ainsi que leur restitution sous forme de données organisées (graphiques, tableaux et logs).
+                    Par ailleurs, lors de mon stage de deuxième année au CIRAD avec le projet <a href="router.php?action=afficherProjet&controleur=base&numProjet=4">Race against the Fly</a>, j'ai manipulé (extraire, convertir et génerer) des données issues de <b>résultats scientifiques</b> pour mettre en œuvre une <b>interface graphique</b> en <i>Python</i> avec des données de configurations (yaml) et des données de modélisaiton (csv). Cette application permet la saisie des données dans un <b>modèle scientifique</b>, l'affichage des résultats via l'interface graphique, ainsi que leur restitution sous forme de données organisées (graphiques, tableaux et logs).
                 </p>
 
                 <p class="competenceDescription">
                     Les cours de base de données m'ont permis de consolider mes connaissances en <b>conception et normalisation de bases
-                    de données relationnelles</b>. Le projet <a href="">Tricount</a> a été une concrétisation de l'optimisation d'un modèle de données préexistant. La normilsation de la base de données pour ce projet a permis de mieux structurer les données afin d'éviter toutes formes d'incohérence et de redondance. L'application des <b>trois formes normales</b> et des <b>théorèmes de Casey Delobel et de Boyce-Codd</b> m'a permis de m'assurer que les données ont bien été conservée durant le precessus de normalisation.
+                    de données relationnelles</b>. Le projet <a href="router.php?action=afficherProjet&controleur=base&numProjet=5">Very Bad Split</a> a été une concrétisation de l'optimisation d'un modèle de données préexistant. La normilsation de la base de données pour ce projet a permis de mieux structurer les données afin d'éviter toutes formes d'incohérence et de redondance. L'application des <b>trois formes normales</b> et des <b>théorèmes de Casey Delobel et de Boyce-Codd</b> m'a permis de m'assurer que les données ont bien été conservée durant le precessus de normalisation.
                 </p>
 
                 <div class="competenceListes">
